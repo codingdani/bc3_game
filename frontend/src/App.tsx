c6663bc0635b6b56db0d79b70eecdc1d2dca89a6
codingdani <96432557+codingdani@users.noreply.github.com>
2023-05-10 17:15:33 +0200

    included infura API support and wallet connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import CreateGame from './Screens/CreateGame';
 import CurrentOpenGames from './Screens/CurrentOpenGames';
 import EnterGame from './Screens/EnterGame';
 import HomeScreen from './Screens/HomeScreen';
 import StartGame from './Screens/StartGame';
-import { infuraKey } from "./utils/interact";
+import { connectWallet, infuraKey } from "./utils/interact";
+
 
 function App() {
+
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [message, setMessage] = useState("No connection to the network."); //default message
+  const [newMessage, setNewMessage] = useState("");
+
+
+  const connectWalletPressed = async () => {
+    const walletResponse: any = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.adress);
+  };
 
   useEffect(() => {
 
@@ -15,7 +28,16 @@ function App() {
 
   return (
     <div className="App">
-      <button id="connectwalletbtn" className="btn">Connect Wallet</button>
+      <button id="connectwalletbtn" className="btn" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
       <HomeScreen></HomeScreen>
     </div>
   );
--- frontend/src/utils/interact.ts
+++ frontend/src/utils/interact.ts
@@ -1,8 +1,34 @@
-export const infuraKey = process.env.REACT_APP_INFURA_KEY;
-console.log("API KEY: ", infuraKey)
+export const infuraKey = "b6b652a41f604aadb527654f04bed96c";
+
+const Web3 = require('web3');
+
+const web3 = new Web3(
+    new Web3.providers.HttpProvider('https://sepolia.infura.io/v3/b6b652a41f604aadb527654f04bed96c')
+);
 
 
 export const connectWallet = async () => {
-
+    if (window.ethereum) {
+        try {
+            const adressArray = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+            const obj = {
+                status: "Connected",
+                adress: adressArray[0],
+            }
+            return obj
+        } catch (err: any) {
+            return {
+                address: "",
+                status: "Uff...Error: " + err.message,
+            }
+        }
+    } else {
+        return {
+            address: "",
+            status: "You must install MetaMask in your Browser",
+        }
+    }
 }
 
