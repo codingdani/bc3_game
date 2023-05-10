8db029ea3cfe2fe005826f8b1c337e9798338c55
codingdani <96432557+codingdani@users.noreply.github.com>
2023-05-10 16:44:25 +0200

    frontend updated - problem with infura key import from .env

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import CreateGame from './Screens/CreateGame';
 import CurrentOpenGames from './Screens/CurrentOpenGames';
 import EnterGame from './Screens/EnterGame';
 import HomeScreen from './Screens/HomeScreen';
 import StartGame from './Screens/StartGame';
+import { infuraKey } from "./utils/interact";
 
 function App() {
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className="App">
       <button id="connectwalletbtn" className="btn">Connect Wallet</button>
-      <StartGame></StartGame>
+      <HomeScreen></HomeScreen>
     </div>
   );
 }
---
+++ frontend/src/utils/interact.ts
@@ -0,0 +1,8 @@
+export const infuraKey = process.env.REACT_APP_INFURA_KEY;
+console.log("API KEY: ", infuraKey)
+
+
+export const connectWallet = async () => {
+
+}
+
