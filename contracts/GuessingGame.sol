0535a08b62e0eda8a9f03aaa1644c86ed5ffbbb1
Tai Nguyen Nhan <tainguyennhan@MacBook-Pro-von-Tai-2.local>
2023-06-14 13:27:45 +0200

    fix: naming

--- contracts/GuessingGame.sol
+++ contracts/GuessingGame.sol
@@ -166,17 +166,16 @@ contract GuessingGame {
         payable(winner).transfer(winningAmount);
     }
 
-    function retrieveWinningFee() external onlyOwner {
+    function retrieveServiceFee() external onlyOwner {
         require(winnerHasWithdrawn, "The winner hasn't payout their win.");
         require(address(this).balance > 0, "You already collected your fee.");
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    function calcWinningDiff(uint256 minDiff, uint256 target)
-        private
-        view
-        returns (uint256)
-    {
+    function calcWinningDiff(
+        uint256 minDiff,
+        uint256 target
+    ) private view returns (uint256) {
         for (uint256 i = 0; i < players.length; i++) {
             if (
                 commits[players[i]].revealed == true &&
@@ -188,11 +187,10 @@ contract GuessingGame {
         return minDiff;
     }
 
-    function absDiff(uint256 num1, uint256 num2)
-        private
-        pure
-        returns (uint256)
-    {
+    function absDiff(
+        uint256 num1,
+        uint256 num2
+    ) private pure returns (uint256) {
         if (num1 >= num2) {
             return num1 - num2;
         } else {
